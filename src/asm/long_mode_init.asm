dba928e322eca9b7d29ca275de0391f7cbe0d851
Glenn Olsson <glennholsson@gmail.com>
2022-04-07 13:21:11 -0700

    Revert to simple splash screen

--- src/asm/long_mode_init.asm
+++ src/asm/long_mode_init.asm
@@ -12,16 +12,6 @@ long_mode_start:
     mov fs, ax
     mov gs, ax
 
-    ; print `OKAY` to screen
-    mov rax, 0x2f6e2f652f6c2f47
-    mov qword [0xb80a0], rax
-    
- 	mov rax, 0x2f212f532f4f2f6e
-    mov qword [0xb80a8], rax
-
-	mov rax, 0x2f212f532f070000
-    mov qword [0xb80b0], rax
-
 	call kmain
 
 	hlt
--- src/c/main.c
+++ src/c/main.c
@@ -12,33 +12,9 @@ void ugly_sleep(int sec) {
 
 void kmain() {
 
-	write_str(5, 0, "Hello my name is Glenn, and I am the absolute best skiier on this mf mountain!");
-	
-	write_to(79, 24, 0x2f44);
+	printk("Hello, welcome to GlennOS!");
 
-	ugly_sleep(5);
-
-	int j = 15;
-
-	while(j--) {
-		ugly_sleep(1);
-		printk("Hello %s, my name is Glenn and I am %d years old. Favourite letter is %c. -10 as unsigned is %u. 45 in hex is %x and j is at address %p %%\n", "Glenne", j, 'g', -10, 45, &j);
-	}
-
-	VGA_display_char('\n');
-	VGA_display_char('\n');
-	VGA_display_char('\n');
-	
-	print_char('!');
-	print_str(" Hej jag heter glenn ");
-	print_short(-79);
-	print_uchar(245);
-	print_long_hex(0x11111);
-
-	printk("\nshort int 50000 == %hd == 0x%hx == %hu\n", 50000, 50000, 50000);
-	printk("long int 10000 == %ld == 0x%lx == %lu\n", 10000, 10000, 10000);
-	printk("char a == %qd == 0x%qx == %qu\n", 'a', 'a', 'a');
-
+	int j = 0;
 	while(!j)
 		asm("hlt");
 }
